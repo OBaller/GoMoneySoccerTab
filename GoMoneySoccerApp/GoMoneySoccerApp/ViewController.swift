e68d367e0b57349d7ad1f3afd20d832f7254d8fd
Naseem Oyebola <naseemoyebola@gmail.com>
2023-05-11 20:18:07 +0100

    Initial commit

---
+++ GoMoneySoccerApp/GoMoneySoccerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoMoneySoccerApp
+//
+//  Created by Naseem Oyebola on 11/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
